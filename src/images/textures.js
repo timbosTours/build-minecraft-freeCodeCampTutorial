d32b07f633aba923160ab0220191350abd443e70
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-10-29 16:04:54 +1100

    added sky, set up textures

---
+++ src/images/textures.js
@@ -0,0 +1,25 @@
+import { TextureLoader } from 'three'
+
+import {
+    dirtImg,
+	grassImg,
+	glassImg,
+	woodImg,
+	logImg,
+} from './images'
+
+const dirtTexture = new TextureLoader(dirtImg)
+const logTexture = new TextureLoader(logImg)
+const grassTexture = new TextureLoader(grassImg)
+const glassTexture = new TextureLoader(glassImg)
+const woodTexture = new TextureLoader(woodImg)
+const groundTexture = new TextureLoader(grassImg)
+
+export {
+    dirtTexture,
+    logTexture,
+    grassTexture,
+    glassTexture,
+    woodTexture,
+    groundTexture
+}
