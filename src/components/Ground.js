085f9dfa5fbb5b1969f0ed4fce5de7ace4c1a197
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-10-30 13:20:12 +1100

    added ground

---
+++ src/components/Ground.js
@@ -0,0 +1,24 @@
+import { usePlane } from "@react-three/cannon";
+import { NearestFilter, RepeatWrapping } from "three";
+import { groundTexture } from "../images/textures"
+
+// ground component
+export const Ground = () => {
+    const [ref] = usePlane(() => ({
+        rotation: [-Math.PI/4, 0, 0], position: [0, 0, 0]
+    }))
+
+    // stop texture from stretching
+    groundTexture.magFilter = NearestFilter;
+    groundTexture.wrapS = RepeatWrapping;
+    groundTexture.wrapT = RepeatWrapping;
+    groundTexture.repeat.set(100,100)
+
+    // return ground geometry with texture wrapped
+    return (
+        <mesh ref={ ref }>
+            <planeBufferGeometry attach='geometry' args={[100, 100]} />
+            <meshStandardMaterial attach='material' map={groundTexture} />
+        </mesh>
+    )
+}
