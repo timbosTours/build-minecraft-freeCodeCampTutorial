48b60a24a9fe012ffb38f969572754b90768413c
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-11-04 08:09:01 +1100

    hover effects

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -7,6 +7,7 @@ import { dirtImg } from "../images/images"
 
 
 export const Cube = ({ position, texture }) => {
+    const [isHovered, setIsHovered] = useState(false)
     const [ref] = useBox(() => ({
         type: 'Static',
         position
@@ -17,6 +18,14 @@ export const Cube = ({ position, texture }) => {
 
     return (
         <mesh
+            onPointerMove={(e) => {
+                e.stopPropagation()
+                setIsHovered(true)
+            }}
+            onPointerOut={(e) => {
+                e.stopPropagation()
+                setIsHovered(false)
+            }}
             onClick={(e) => {
                 e.stopPropagation()
                 const clickedFace = Math.floor(e.faceIndex / 2)
@@ -56,7 +65,12 @@ export const Cube = ({ position, texture }) => {
             }}
             ref={ref}>
             <boxBufferGeometry attach="geometry"/>
-            <meshStandardMaterial map={activeTexture} attach="material"/>
+            <meshStandardMaterial
+                color={isHovered ? 'grey' : 'white'}
+                map={activeTexture}
+                transparent={true}
+                opacity={texture === 'glass' ? 0.6 : 1}
+                attach="material" />
         </mesh>
     )
 }
